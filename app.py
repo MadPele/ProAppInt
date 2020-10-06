a48cb5d7070f98db3e7469954356dda35325b6dc
Andrzej Derela <andrzejderela@gmail.com>
2020-10-06 20:00:05 +0200

    Day1 - home page v0.1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/home')
+def home_page():
+    return render_template('home_page.html')
 
 
 if __name__ == '__main__':
